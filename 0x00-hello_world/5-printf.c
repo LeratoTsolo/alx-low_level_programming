5c1a13941673b79be84709ad8aa09e78837f76de
LeratoTsolo <LeratoTsolo4@gmail.com>
2023-07-16 15:09:36 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-/*
- * Main - Prints hello world using printf.
+/**
+ * Main - Point of entry
 */
 int main(void)
 {
-	printf("with proper grammar,but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
