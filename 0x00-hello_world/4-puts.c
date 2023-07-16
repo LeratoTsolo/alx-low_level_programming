2280be425df40b38b0f9b626bb40d7808e215c6e
LeratoTsolo <LeratoTsolo4@gmail.com>
2023-07-16 14:50:57 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/*
+ *main functions prints the specified words using puts not printf.
+ *Return value is 0
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
