5c5faa84c70bc094566f16efda31f96486bafdea
LeratoTsolo <LeratoTsolo4@gmail.com>
2023-07-17 14:39:06 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
 */
 int main(void)
 {
-	printf("Size of char\:", sizeof(char);
-	printf("Size of int\:", sizeof(int);
-	printf("Size of long int\:", sizeof(long int);
-	printf("Size of long long int\:", sizeof(long long int);
-	printf("Size of float\:", sizeof(float);
+	printf("Size of char:", sizeof(char);
+	printf("Size of int:", sizeof(int);
+	printf("Size of long int:", sizeof(long int);
+	printf("Size of long long int:", sizeof(long long int);
+	printf("Size of float:", sizeof(float);
 	return(0);
 	}	
