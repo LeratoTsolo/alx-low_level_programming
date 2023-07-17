6fdf7d727baf68104fce2cd841a6eef43eb07cc3
LeratoTsolo <LeratoTsolo4@gmail.com>
2023-07-17 14:36:16 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - 
+* Return: (0) success
+*/
+int main(void)
+{
+	printf("Size of char\:", sizeof(char);
+	printf("Size of int\:", sizeof(int);
+	printf("Size of long int\:", sizeof(long int);
+	printf("Size of long long int\:", sizeof(long long int);
+	printf("Size of float\:", sizeof(float);
+	return(0);
+	}	
