eb36b6bf7e3e49632ee8948697823a1b0b3fd4b5
LeratoTsolo <LeratoTsolo4@gmail.com>
2023-07-18 12:38:12 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "_putchar.c"
+/**
+*main - prints _putchar followed by new line.
+*Return: (0) success
+*/
+int main(void)
+{
+	char c[9] = "_putchar";
+	int i =1;
+	for(i = 1; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
+	return (0);
+}
