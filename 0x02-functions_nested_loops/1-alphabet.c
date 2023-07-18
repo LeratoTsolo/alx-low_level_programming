24ecc0becebd1ddf9960bc3b1a9a421fed279ac5
LeratoTsolo <LeratoTsolo4@gmail.com>
2023-07-18 16:21:11 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* print_alphabet - print alphabets in lowecase.
+* Return: (0) success
+*/
+void print_alphabet(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+}
