43506bcf070f6c6c92e9e58f72c21c459d7ab712
LeratoTsolo <LeratoTsolo4@gmail.com>
2023-07-19 16:50:16 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,14 +2,15 @@
 #include <ctype.h>
 /**
 *_islower - check for lowercase charactr
-*Return: (1) or (0)
+*@c: parameter
+ *Return: (1) or (0)
 */
 int _islower(int c)
 {
-	if (c > 'a' && c <= 'z')
-	{
-	return (1);
-	}
-	else
-	return(0);
+        if (c > 'a' && c <= 'z')
+        {
+        return (1);
+        }
+        else
+        return(0);
 }
