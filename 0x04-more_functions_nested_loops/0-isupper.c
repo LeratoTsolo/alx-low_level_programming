7b9fd9941a84410eb72f37aedf4ba2cc8c4f25de
LeratoTsolo <LeratoTsolo4@gmail.com>
2023-07-20 14:06:07 -0700

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <ctype.h>
+/**
+*_islower - check for lowercase charactr
+*@c: parameter
+*Return: (1) or (0)
+*/
+int _isupper(int c)
+{
+        if (c > 'A' && c <= 'Z')
+        {
+        return (1);
+        }
+        else
+        return (0);
+}
