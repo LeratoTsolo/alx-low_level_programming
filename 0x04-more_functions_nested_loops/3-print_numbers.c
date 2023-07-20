e165d85a6e0b6abc496b130d1f141d4cc0d1702b
LeratoTsolo <LeratoTsolo4@gmail.com>
2023-07-20 14:57:08 -0700

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_numbers - print alphabets in lowecase.
+* Return: (0) success
+*/
+void print_numbers(void)
+{
+        int i;
+
+        for (i = 0; i <= 9; i++)
+        {
+        _putchar(i + '0');
+        }
+        _putchar('\n');
+}
