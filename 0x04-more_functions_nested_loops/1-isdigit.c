1a8061ef30555aeb063ff8b394093889fa4957a5
LeratoTsolo <LeratoTsolo4@gmail.com>
2023-07-20 14:24:26 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -7,10 +7,8 @@
 */
 int _isdigit(int c)
 {
-        if (isdigit(c) == 0)
-        {
-        return (0);
-        }
-        else
-        return (1);
+	if (isdigit(c) == 0)
+	return (0);
+	else
+	return (1);
 }
