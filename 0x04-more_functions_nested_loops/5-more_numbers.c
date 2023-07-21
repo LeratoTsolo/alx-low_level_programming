fa0ee395621df0cc191ded2dea5c01969d6eac48
LeratoTsolo <LeratoTsolo4@gmail.com>
2023-07-20 18:10:02 -0700

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_most_numbers - print alphabets in lowecase.
+*/
+void print_most_numbers(void)
+{
+        int i;
+	int a;
+
+	for (a = 0; a <= 10; a++)
+	{
+	for (i = 0; i <= 14; i++)
+	{
+	_putchar(i + '0');
+	}
+	_putchar('\n');
+        }
+}
