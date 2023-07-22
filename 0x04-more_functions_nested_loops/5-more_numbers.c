1433a764337442b3cc01a11ffc7205fcfbaffd19
LeratoTsolo <LeratoTsolo4@gmail.com>
2023-07-21 17:20:45 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
 *more_numbers - print alphabets in lowecase.
+*return: void
 */
 void more_numbers(void)
 {
@@ -12,7 +13,7 @@ void more_numbers(void)
 		for (i = 0; i <= 14; i++)
 		{
 			if (i > 9)
-			_putchar(i / 10 +'0');
+			_putchar(i / 10 + '0');
 			_putchar(i % 10 + '0');
 		}
 		_putchar('\n');
