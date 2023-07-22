15390747ddeac29b5527e0ece839ffcb4f674acf
LeratoTsolo <LeratoTsolo4@gmail.com>
2023-07-21 18:42:40 -0700

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*main - fizz buzz
+*Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		printf("%s ", "Fizzbuzz");
+		else if (i % 5 == 0)
+		printf("%s ", "Buzz");
+		else if (i % 3 == 0)
+		printf("%s ", "Fizz");
+		else
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
