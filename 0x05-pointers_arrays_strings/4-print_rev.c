f03f2296f192b9f992e21486b89bdd1d5da73856
LeratoTsolo <LeratoTsolo4@gmail.com>
2023-07-25 12:52:14 -0700

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,9 +9,10 @@ void print_rev(char *s)
 	int c;
 	int i;
 
+	c = 0;
 	for (i = 0; s[i] != '\0'; i++)
 	c++;
-	for (c = c; c >= 0; c--)
+	for (c = c - 1; c >= 0; c--)
 	_putchar(s[c]);
 	_putchar('\n');
 }
