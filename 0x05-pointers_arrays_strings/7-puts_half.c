f8c957c13a7a7980ac445a8bd09f3ddfd24b4926
LeratoTsolo <LeratoTsolo4@gmail.com>
2023-07-25 14:59:50 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,16 +6,11 @@
 */
 void puts_half(char *str)
 {
-	int i;
-	int c;
-	int s = 0;
-		
+	int i, c, s = 0;
+			
 	for (i = 0; str[i] != '\0'; i++)
 	c++;
-/*	if (c % 2 == 0)
-	i = c / 2;
-	else
-*/	s = (c - 1) / 2;
+	s = (c - 1) / 2;
 	for (i = s + 1; str[i] != '\0'; i++)
 	_putchar(str[i]);
 	_putchar('\n');
