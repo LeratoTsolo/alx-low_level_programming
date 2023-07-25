4267a55c02af85c3a3db8ba6f1267644a7860a04
LeratoTsolo <LeratoTsolo4@gmail.com>
2023-07-25 12:45:29 -0700

    0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*print_rev - print a string in reverse
+*@s: input string
+*Return: void
+*/
+void print_rev(char *s)
+{
+	int c;
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	c++;
+	for (c = c; c >= 0; c--)
+	_putchar(s[c]);
+	_putchar('\n');
+}
