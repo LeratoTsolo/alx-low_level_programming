1716e100f2997892a8216dc5e73ebe29f2a24fde
LeratoTsolo <LeratoTsolo4@gmail.com>
2023-07-25 14:01:31 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,19 +6,17 @@
 */
 void rev_string(char *s)
 {
-        int c;
-        int i;
+	int c;
+	int i;
 	char st;
 
-        c = 0;
-        for (i = 0; s[i] != '\0'; i++)
-        c++;
-        for (i = 0 ; i < c / 2; i++)
+	c = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	c++;
+	for (i = 0 ; i < c / 2; i++)
 	{
 	st = s[i];
- 	s[i] = s[c - 1 - i];
+	s[i] = s[c - 1 - i];
 	s[c - 1 - i] = st;
 	}	
-		/*_putcha(s[c]);
-        _putchar('\n'); */
 }
