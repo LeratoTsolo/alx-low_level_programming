e2a316f5b3b900f9def082d0046d0e7383f91ed2
LeratoTsolo <LeratoTsolo4@gmail.com>
2023-07-28 14:32:14 -0700

    0x06-pointer_arrays_string

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+*_strncat - concatinate two strings
+*@dest: destination string
+*@src: source string
+*Return: character
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int l;
+	int i;
+
+	l = strlen(dest);
+	for (i = 0; i < n && *src != '\0'; i++)
+	{
+	dest[l + i] = *src;
+	src++;
+	}
+	dest[l + 1] = '\0';
+	return (dest);
+}
