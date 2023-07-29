52d721ade9ec626ac7faa6d2e0a55b79015c9bc4
LeratoTsolo <LeratoTsolo4@gmail.com>
2023-07-28 19:17:49 -0700

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "_putchar.c"
+/**
+*reverse_array - print a string in reverse
+*@a: input int
+*@n: input int
+*Return: void
+*/
+void reverse_array(int *a, int n)
+{
+	int c;
+	int i;
+	
+	n = n -1;
+	for (i = 0; i < n; i++)
+	{
+	c = a[i];
+	a[i] = a[n];
+	a[n] = c;
+	n--;	
+	}
+} 
