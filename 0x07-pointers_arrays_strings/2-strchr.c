16df1ebb358317144c5cba9be0eb277cf53c7b54
LeratoTsolo <LeratoTsolo4@gmail.com>
2023-07-31 11:01:35 -0700

    0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*_strchr - locate charactcer in string
+*@s: input
+*@c: input
+*Return: zero
+*/
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (;s[i] >= '\0'; i++)
+	{
+	if (s[i] == c)
+	return (&s[i]);
+	}
+	return(0);
+}
