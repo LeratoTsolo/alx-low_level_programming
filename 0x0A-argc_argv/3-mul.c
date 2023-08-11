340729b0b754d9930056b7624a04742c4b2ae274
LeratoTsolo <LeratoTsolo4@gmail.com>
2023-08-11 12:11:20 -0700

    0x0A-argc_argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,41 +3,41 @@
 /**
 *_atoi - convert char to int
 *@s: input char
-*Retur: value
+*Return: value
 */
 int _atoi(char *s)
 {
-        int i, d, l, n, f, result;
+	int i, d, l, n, f, r;
 
-        i = 0;
-        d = 0;
-        n = 0;
-        f = 0;
-        l = 0;
-        result = 0;
+	i = 0;
+	d = 0;
+	n = 0;
+	f = 0;
+	l = 0;
+	r = 0;
 
-        while (s[l] != '\0')
-                l++;
-        while (i < l && f == 0)
-        {
-                if (s[i] == '-')
-                d++;
-                if (s[i] >= '0' && s[i] <= '9')
-                {
-                        result = s[i] - '0';
+	while (s[l] != '\0')
+	l++;
+	while (i < l && f == 0)
+	{
+		if (s[i] == '-')
+		d++;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			r = s[i] - '0';
                         if (d % 2)
-                        result = result;
-                        n = n * 10 + result;
-                        f = 1;
-                        if (s[1 + 1] < '0' || s[i + 1] > '9')
-                        break;
-                        f = 0;
-                }
-                i++;
-        }
-                if (f == 0)
-                return (0);
-                return (n);
+			r = -r;
+			n = n * 10 + r;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+			break;
+			f = 0;
+		}
+		i++;
+	}
+	if (f == 0)
+	return (0);
+	return (n);
 }
 
 /**
